26e03946c6625503a2e1a0bdf3f71f7b7e2a3e41
Igorsantos1996 <igorsantos1996@gmail.com>
2020-02-06 10:54:09 -0200

    botao de ok e numero de participantes

--- src/APP.java
+++ src/APP.java
@@ -3,14 +3,16 @@ import javax.swing.border.LineBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.net.URL;
 import java.text.DateFormat;
 import java.util.Date;
 
 
 public class APP implements ActionListener {
-    private JLabel numeroContribuintes;
-    private JTextField campodata;
+    private JLabel contribuintes;
+    private JTextField campodata, numeroContribuintes;
     private JMenuBar mnBarra;
     private JMenu mnSobre;
     private Image iconeTitulo;
@@ -23,16 +25,18 @@ public class APP implements ActionListener {
     //private JMenu mnExportar;
     //private JMenu miSair;
     private JFrame jFramePrincipal;
-    private JButton botaoIniciar;
+    private JButton botaoIniciar, feito;
 
 
     public APP() {
         jFramePrincipal = new JFrame("Real time codding");
         mnBarra = new JMenuBar();
         //miSair = new JMenu("Sair");
-        numeroContribuintes = new JLabel();
+        contribuintes = new JLabel("Quantidade de participantes");
+        numeroContribuintes = new JTextField();
         campodata = new JTextField();
         mnSobre = new JMenu("Sobre");
+        feito = new JButton("Feito");
         //mnExportar  = new JMenu("Exportar Classe java");
         botaoIniciar = new JButton("Iniciar");
         miAutores = new JMenuItem("Info autores");
@@ -58,8 +62,11 @@ public class APP implements ActionListener {
         botaoIniciar.addActionListener(this);
         mnSobre.add(miAutores);
         miAutores.addActionListener(this);
+        feito.addActionListener(this);
         jFramePrincipal.setJMenuBar(mnBarra);
         jFramePrincipal.add(numeroContribuintes);
+        jFramePrincipal.add(contribuintes);
+        jFramePrincipal.add(feito);
 
         jFramePrincipal.add(botaoIniciar);
         URL path = this.getClass().getResource("programming-flag.png");
@@ -67,8 +74,12 @@ public class APP implements ActionListener {
 
         jFramePrincipal.setIconImage(iconeTitulo);
 
-        botaoIniciar.setBounds(380, 500, 150, 30);
-        numeroContribuintes.setBounds(70, 70, 200, 30);
+        botaoIniciar.setBounds(380, 520, 150, 30);
+
+        botaoIniciar.setEnabled(false);
+        numeroContribuintes.setBounds(380, 470, 150, 30);
+        contribuintes.setBounds(180, 470, 200, 30);
+        feito.setBounds(540, 470, 100, 30);
         jFramePrincipal.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         jFramePrincipal.getContentPane().setLayout(null);
         jFramePrincipal.setPreferredSize(new java.awt.Dimension(900, 650));
@@ -81,10 +92,14 @@ public class APP implements ActionListener {
         //System.out.println(formatador.format(data));
         campodata.setText(sdf.format(data));
         campodata.setBounds(730, 10, 100, 50);
-        campodata.setBorder(new LineBorder(Color.WHITE, 0,false));
+        campodata.setBorder(new LineBorder(Color.WHITE, 0, false));
         campodata.setFont(new Font("arial", Font.BOLD, 16));
         jFramePrincipal.add(campodata);
         campodata.setEnabled(false);
+        //feito.setEnabled(false);
+        contribuintes.setFont(new Font("arial", Font.CENTER_BASELINE, 14));
+        numeroContribuintes.setFont(new Font("arial", Font.CENTER_BASELINE, 14));
+        jFramePrincipal.getRootPane().setDefaultButton(botaoIniciar);
 
     }
 
@@ -93,8 +108,15 @@ public class APP implements ActionListener {
     public void actionPerformed(ActionEvent e) {
 
 
-        if (e.getSource().equals(botaoIniciar)) {
+        if ((!numeroContribuintes.getText().trim().isEmpty()) && (e.getSource().equals(feito))) {
+            System.out.println(numeroContribuintes.getText());
+            botaoIniciar.setEnabled(true);
+            feito.setEnabled(false);
+            numeroContribuintes.setEditable(false);
 
+        }
+
+        if (e.getSource().equals(botaoIniciar)) {
         } else if (e.getSource().equals(miAutores)) {
             JOptionPane.showMessageDialog(null, "Ainda não implementado");
         }
