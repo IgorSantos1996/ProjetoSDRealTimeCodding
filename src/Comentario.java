092ddd9c59f83cd008ead2fae3997d4c6cfed8ec
Clovis22 <clovis_jack@hotmail.com>
2020-02-19 08:22:21 -0300

    Implementando classe Comentario.java

--- src/Comentario.java
+++ src/Comentario.java
@@ -8,27 +8,38 @@ public class Comentario implements ActionListener {
 
     public JFrame      JComentario;
     public JPanel      JPainel;
-    public JTextArea   JTextArea;
+    public JTextArea   JTextArea1, JTextArea2;
     public JScrollPane JBarraDeRolagem;
     public JButton     btnEnviar;
 
     public Comentario() {
         JComentario = new JFrame("Comentário");
-        JTextArea = new JTextArea(15, 50);
-        JPainel = new JPanel();
+        JTextArea1  = new JTextArea(15, 50);
+        JBarraDeRolagem = new JScrollPane(JTextArea1);
+      //  btnEnviar   = new JButton("Enviar");
+        //JTextArea2  = new JTextArea(15,50);
+        //JPainel     = new JPanel();
         inicializarComponentes();
     }
 
     public void inicializarComponentes() {
 
-        btnEnviar.addActionListener(this);
+        //btnEnviar.addActionListener(this);
 
-        JBarraDeRolagem.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-        JBarraDeRolagem.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+//        JBarraDeRolagem.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+  //      JBarraDeRolagem.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+        //JBarraDeRolagem.add(JTextArea1);
+        //JComentario.add(btnEnviar);
+        JComentario.getContentPane().add(JBarraDeRolagem);
+        //JComentario.add(JTextArea1);
 
-        JPainel.add(JBarraDeRolagem);
-        JPainel.add(btnEnviar);
-        JComentario.getContentPane().add(JPainel);
+
+
+    //    btnEnviar.setBounds(50,50,100,100);
+        JTextArea1.setBounds(15,15, 850, 300);
+
+        JComentario.getContentPane().setLayout(null);
+        JComentario.setPreferredSize(new Dimension(900, 650));
 
         JComentario.pack();
         JComentario.setLocationRelativeTo(null);
